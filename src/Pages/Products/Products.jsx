d7302fd4bd96422db7b13e4b8175d8af5e5ae77d
rakibjrb <rakibjrb572157@gmail.com>
2023-11-28 21:14:07 +0600

    added pagination system in products page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,22 +1,43 @@
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import SectionHeader from "../../Components/SectionHeader/SectionHeader";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import Card from "../../Components/ProductCard/Card";
-import { useEffect, useState } from "react";
 import NotFound from "../../Components/Common/NotFound";
 import useGetAllVotes from "../../Hooks/useGetAllVotes";
 
 const Products = () => {
-  let count = 0;
-  const axios = useAxiosPublic();
+  const [currentPage, setCurrentPage] = useState(0);
   const [allProducts, setAllProducts] = useState([]);
   const [searchText, setSearchText] = useState("");
   const { votedproduct } = useGetAllVotes();
+
+  let count = 0;
+  const axios = useAxiosPublic();
+  const { data: totalproducts = 1 } = useQuery({
+    queryKey: ["documentcount"],
+    queryFn: async () => {
+      const res = await axios.get("/document-count");
+      return res.data.totalDocuments;
+    },
+  });
 
   const { data, isPending } = useQuery({
     queryKey: ["all_products", searchText],
     queryFn: async () => {
       const res = await axios.get(`/product/all?search=${searchText}`);
+      return res.data;
+    },
+  });
+
+  const perpage = 8;
+
+  const { data: paginationdata } = useQuery({
+    queryKey: ["paginationproducts", currentPage],
+    queryFn: async () => {
+      const res = await axios.get(
+        `/pagination?skip=${currentPage * 5}&limit=${perpage}`
+      );
       return res.data;
     },
   });
@@ -27,9 +48,16 @@ const Products = () => {
     setSearchText(searchedText);
   };
 
+  const pagescreate = Math.ceil(totalproducts / perpage);
+  const pages = [...Array(pagescreate).keys()];
+
   useEffect(() => {
     setAllProducts(data);
   }, [data]);
+
+  useEffect(() => {
+    setAllProducts(paginationdata);
+  }, [paginationdata]);
 
   return (
     <div className="max-w-7xl mx-auto px-3 xl:px-0 mt-16 mb-24">
@@ -61,6 +89,37 @@ const Products = () => {
           ))
         )}
       </div>
+
+      <div className="mt-8 max-w-3xl mx-auto flex justify-center flex-wrap space-x-2 px-3 xl:px-0">
+        <button
+          disabled={currentPage === 0}
+          className="btn btn-sm"
+          onClick={() => {
+            setCurrentPage(currentPage - 1);
+          }}
+        >
+          Prev
+        </button>
+        {pages?.map((page) => (
+          <button
+            key={`paginationbtn${page}`}
+            className={`${currentPage === page && "bg-red-400"} btn btn-sm`}
+            onClick={() => setCurrentPage(page)}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          disabled={currentPage === pages?.length - 1}
+          className="btn btn-sm"
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+
       <div className={`${allProducts?.length ? "hidden" : ""}`}>
         <NotFound />
       </div>
